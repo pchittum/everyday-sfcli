94e63e35db6080b2d984ecc5f9840b26e7793e77
Peter Chittum <pchittum@salesforce.com>
2018-09-22 09:38:46 -0700

    mac based shell scripts for limits parsing

---
+++ limits/limits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#capture stdout
+dxoutput=$(sfdx force:limits:api:display -u blixtar)
+
+#scriptout=$($dxoutput > grep -v 'Hourly')
+
+#this writes to stdout for this script
+echo date
+echo "${dxoutput}"
---
+++ limits/usagelog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#capture stdout
+dxoutput=$(sfdx force:limits:api:display -u blixtar | grep -v 'Hourly' | grep -v 'Concurrent' | grep -v 'Daily' | grep -v 'Email' | grep -v 'Package')
+
+#scriptout=$($dxoutput > grep -v 'Hourly')
+
+#this writes to stdout for this script
+echo date >> log-output/usage.log
+echo "${dxoutput}" >> log-output/usage.log
